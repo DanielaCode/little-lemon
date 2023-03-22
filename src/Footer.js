96fbb50a53fa89dd6d8abe33c3136227550152ac
DanielaCode <danielagp96@gmail.com>
2023-03-21 20:13:27 -0600

    Fist HTML5 skeleton and adding some meta tags

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <img/>
+            </div>
+            <div>
+              <div>
+                
+              </div>
+              <div>
+                
+              </div>
+              <div>
+                
+              </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a>Order Online</a></li>
+                <li><a>About</a></li>
+                <li><a>Home</a></li>
+                <li><a>Login</a></li>
+                <li><a>Reservations</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
