d5f0e3dde369287dc01ff09fe6f692044015aedd
Daniela <danielagp96@gmail.com>
2023-04-01 20:36:07 -0600

    Listing cards content and reusing the card component to show it

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from "../styles/Card.module.css";
-import img from "../assets/lemon.jpg"
-export default function Card() {
+
+export default function Card(props) {
   return (
     <div className={styles.card}>
-      <img className={styles.img} src={img}/>
+      <img className={styles.img} src={props.path}/>
       <div className={styles.infoContainer}>
         <div className={styles.titlePrice}>
-          <p className='card-title-text'>title</p>
-          <p className='paragraph-card-text'>$12.99</p>
+          <p className='card-title-text'>{props.title}</p>
+          <p className='paragraph-card-text'>{props.price}</p>
         </div>
-        <p className='paragraph-text'>Lorem ipsum dolor sit amet consectetur. Sagittis consequat convallis eget ut faucibus nec eu fringilla egestas. Nibh suscipit magna blandit ves.</p>
+        <p className='paragraph-text'>{props.text}</p>
         <p className='highligth-text'>Order a delivery <span>+</span></p>
       </div>
     </div>
--- src/components/HighLights.js
+++ src/components/HighLights.js
@@ -2,7 +2,44 @@ import React from 'react'
 import styles from "../styles/HighLights.module.css";
 import Button from "./Button";
 import Card from "./Card";
+import lemon from "../assets/lemon.jpg";
+import grekSalad from "../assets/grekSalad.jpg";
+import restaurantFood from "../assets/restaurantFood.jpg";
+import restaurantchef from "../assets/restaurantchef.jpg";
+
 export default function HighLights() {
+  
+  const infoCards=[
+    {
+      path:lemon,
+      title:"Lemon Desert",
+      price:"12.99$",
+      text:"Lorem ipsum dolor sit amet consectetur. Sagittis consequat convallis eget ut faucibus nec eu fringilla egestas. Nibh suscipit magna blandit ves"
+    },
+    {
+      path: grekSalad,
+      title:"Greek Salad",
+      price:"19.0$",
+      text:"Lorem ipsum dolor sit ametit magna blandit consectetur. Sagittis consequat convallis eget ut faucibus nec eu fringilla egestas. Nibh suscipit magna blandita blandit ves"
+    },
+    {
+      path:restaurantFood,
+      title:"Tapas",
+      price:"10.99$$",
+      text:"Lorem ipsum dolor sit ametit magna blandit conseit magna blanditctetur. Sagittis consequat convallis eget ut faucibus nec eu fringilla egestas. Nibh suscipit magna blandit ves"
+    },
+    {
+      path:restaurantchef,
+      title:"Salad",
+      price:"18.99$",
+      text:"Lorem ipsum dolor sit amet consectetur. Sagittis consequat convallis eget ut faucibus nec eu fringilla egestas. Nibh suscipit magna blandit ves Nibh suscipit magna blandit ves"
+    },
+  ]
+
+  const cardList = infoCards.map(card =>
+      <Card path={card.path} title={card.title} price={card.price} text={card.text}/>
+);
+
   return (
     <div className={styles.highLights}>
       <div className={styles.orderMenu}>
@@ -10,7 +47,7 @@ export default function HighLights() {
         <Button text="Order Menu"/>
       </div>
       <div className={styles.cards}>
-        <Card/>
+         {cardList}
       </div>
     </div>
   )
