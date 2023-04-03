9a9508a61205a031a20ac11c41476c22d106594b
Daniela <danielagp96@gmail.com>
2023-04-03 13:23:04 -0600

    changes on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import styles from "../styles/Footer.module.css";
+import logo from "../assets/logolarge.png";
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                <img/>
-            </div>
-            <div>
-              <div>
-                
-              </div>
-              <div>
-                
-              </div>
-              <div>
-                
-              </div>
+        <footer className={styles.footer}>
+            <img src={logo} className={styles.img}/>
+            <div className={styles.info}>
+                <div className={styles.nav}>
+                    <h3>Navegation</h3>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Reservations</p>
+                    <p>Login</p>
+                </div>
+                <div className={styles.contact}>
+                    <h3>Contact</h3>
+                    <p>Adress</p>
+                    <p>Phone number</p>
+                    <p>Email</p>
+                </div>
+                <div className={styles.sm}>
+                    <h3>Social Media Links</h3>
+                    <p>Instagram</p>
+                    <p>Twitter</p>
+                    <p>Facebook</p>
+                </div>
             </div>
         </footer>
     );
