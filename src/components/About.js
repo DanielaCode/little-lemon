cbcd9affbfbaba9763366fbb9d3d04c0f8812fde
Daniela <danielagp96@gmail.com>
2023-04-03 08:38:56 -0600

    finishing about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,55 @@
 import React from 'react'
 import styles from "../styles/About.module.css";
+import SmallCard from './SmallCard';
+import uno from "../assets/1.png";
+import dos from "../assets/2.png";
+import tres from "../assets/3.png";
+import cuatro from "../assets/4.png";
+import cinco from "../assets/5.png";
+
 export default function About() {
+
+  const infoCards=[
+    {
+      name:"Dani",
+      img:uno,
+      stars:5,
+      comments:"t convallis eget ut faucibus nLorem ipsrsit ame",
+    },
+    {
+      name:"Sofi",
+      img:dos,
+      stars:4,
+      comments:"Lllis eget ut faucibus nLorem ipsum dols",
+    },
+    {
+      name:"Juan",
+      img:tres,
+      stars:5,
+      comments:"Lorem uat convallis eget ut faucibus n",
+    },
+    {
+      name:"Vale",
+      img:cuatro,
+      stars:5,
+      comments:"Sagittis consequat convallis egen",
+    },
+    {
+      name:"Bibi",
+      img:cinco,
+      stars:5,
+      comments:"Lorem ipsum dolor sit amet consectt",
+    },
+  ]
+  const cardList = infoCards.map(card=>
+        <SmallCard name={card.name} img={card.img} stars={card.stars} comments={card.comments}/>
+    );
   return (
     <div className={styles.about}>
-      About
+      <p className={`${styles.title} diplay2-text`}>What customers say!</p>
+      <div className={styles.cardsContainer}>
+          {cardList}
+      </div>
     </div>
   )
 }
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import styles from "../styles/SmallCard.module.css";
-export default function SmallCard() {
+import star from "../assets/star.png";
+export default function SmallCard(props) {
+  let starts = [];
+
+  for (let i = 0; i < props.stars; i++) {
+    starts.push(<img src={star}/>);
+  }
+
   return (
-    <div>SmallCard</div>
+    <div className={styles.card}>
+        <div className={styles.startsContainer}>
+          {
+            starts
+          }
+        </div>
+        <div className={styles.user}>
+            <img src={props.img}/>
+            <p className='paragraph-text'>{props.name}</p>
+        </div>
+        <p className="paragraph-text">
+          {props.comments}
+        </p>
+    </div>
   )
 }
