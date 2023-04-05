1ca20eea6c7696b7378f05a8463fe8fbcfd87b82
Daniela <danielagp96@gmail.com>
2023-04-05 09:59:23 -0600

    menu responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import Logo from "../assets/Logo.svg";
 import styles from "../styles/Header.module.css";
+
 const Header = () => {
+
     return (
         <header>
             <img className={styles.img} src={Logo}/>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../styles/Nav.module.css";
 import {Link} from 'react-router-dom';
+import arrow from "../assets/down.png";
+
 const Nav = () => {
+    const [menu,setMenu] = useState(false);
+    const handleOnClick=()=>{
+        setMenu(!menu)
+        console.log(menu);
+    }
     return (
         <nav>
-            <ul>
+            <div className={styles.menu} onClick={handleOnClick}>
+                Menu
+                <img src={arrow} className={menu && styles.menuHover}/>
+            </div>
+            <ul className={menu && styles.navVisible}>
                 <li><Link className='header-link-text'>Order Online</Link></li>
                 <li><Link className='header-link-text'>About</Link></li>
                 <li><Link to="/" className='header-link-text'> Home</Link></li>
